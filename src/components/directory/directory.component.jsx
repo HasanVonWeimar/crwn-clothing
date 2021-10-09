adb25ca0f0403ca13f43baf33b4cc3cacf7172b5
AlHasan AlQaraghuli <alhasan_alaaeldin@yahoo.com>
2021-10-09 22:12:19 +0400

    homepage broken into smaller components finished styling menu items

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './directory.styles.scss'
+import MenuItem from '../menu-item/menu-item.component';
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            //we are treating each component of the homepage as it's own section
+             sections: [
+                {
+                    title: 'hats',
+                    imageUrl: 'https://images.unsplash.com/photo-1595642527925-4d41cb781653?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
+                    id: 1,
+                    linkUrl: 'shop/hats'
+                },
+                {
+                    title: 'jackets',
+                    imageUrl: 'https://images.unsplash.com/photo-1591213954196-2d0ccb3f8d4c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
+                    id: 2,
+                    linkUrl: 'shop/jackets'
+                },
+                {
+                    title: 'sneakers',
+                    imageUrl: 'https://images.unsplash.com/photo-1607522370275-f14206abe5d3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=821&q=80',
+                    id: 3,
+                    linkUrl: 'shop/sneakers'
+                },
+                {
+                    title: 'womens',
+                    imageUrl: 'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+                    size: 'large',
+                    id: 4,
+                    linkUrl: 'shop/womens'
+                },
+                {
+                    title: 'mens',
+                    imageUrl: 'https://images.unsplash.com/photo-1512353087810-25dfcd100962?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
+                    size: 'large',
+                    id: 5,
+                    linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({title, id, imageUrl, size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl}
+                        size={size} />
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
